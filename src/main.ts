d513810d9396b54dc467b2127532f722540285b0
Sampath <sampaththaranga13@gmal.com>
2020-04-27 21:02:07 +0530

    all are done and deployment completed

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,22 @@
+// ┌───────────────────────────────────────────────────────────────────────────────────────────────────────┐ \\
+// │ Powerd by : Sampath Tharanga                                                                          │ \\
+// ├───────────────────────────────────────────────────────────────────────────────────────────────────────┤ \\
+// │ Email     : sampaththaranga13@gmail.com                                                               │ \\
+// │ Website   : http://www.athaebula.com/                                                                 │ \\ 
+// │ Facebook  : https://www.facebook.com/sampath.tharanga.50                                              │ \\
+// │ Twitter   : https://twitter.com/sampath13331                                                          │ \\
+// │ Linkdin   : https://www.linkedin.com/in/sampath-tharanga-b842b869/                                    │ \\
+// │ Github    : https://github.com/SampathTharanga                                                        │ \\
+// │                                                                                                       │ \\
+// │ Project   : LMCC System                                                                               │ \\
+// │ Version   : 1.0.0.0                                                                                   │ \\
+// │ Release   : 2020.05.05                                                                                │ \\
+// ├───────────────────────────────────────────────────────────────────────────────────────────────────────┤ \\
+// │ SAMPATH THARANGA | SRMST SAMARADIWAKARA                                            Copyright © 2020   │ \\
+// └───────────────────────────────────────────────────────────────────────────────────────────────────────┘ \\
+
+
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
@@ -10,3 +29,4 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
